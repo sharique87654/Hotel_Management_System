457dcbb96acb1c9bdb0f20f3ef2075f507c26776
Sadique Ali <sadiqueali7488@gmail.com>
2025-11-02 19:01:27 +0530

    Merge branch 'main' of https://github.com/sharique87654/Hotel_Management_System

--- Frontend/UI/src/main.jsx
+++ Frontend/UI/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RoomProvider } from "./context/RoomContext";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RoomProvider } from "./Context/RoomContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RoomProvider>
-    <App />
+      <App />
     </RoomProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
