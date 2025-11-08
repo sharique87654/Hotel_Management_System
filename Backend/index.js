cc4e60cbf2729d86b69f831d6aff1bf57f96e816
sharique <sharique.devs@gmail.com>
2025-11-08 14:56:01 +0530

    added AdminCancelBooking Route

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const usersignup = require("./Auth/routes/signup.js")
 const usersignin = require("./Auth/routes/signin.js")
@@ -8,47 +7,28 @@ const deleteRooms = require("./Admin/deleteRooms.js")
 const updateRooms = require("./Admin/updateRooms.js")
 const bookingRouter = require("./Users/booking.js")
 const addRooms = require("./Admin/addRooms.js")  
-const viewBookings = require("./Admin/viewBooking.js");
+const adminViewBookings = require("./Admin/viewBooking.js");
+const adminCancelBooking = require("./Admin/cancelBooking.js")
 
-=======
-const express = require("express");
-const usersignup = require("./Auth/routes/signup.js");
-const usersignin = require("./Auth/routes/signin.js");
-const adminauth = require("./Admin/auth.js");
-const allHotelData = require("./Admin/allHotelData.js");
-const deleteRooms = require("./Admin/deleteRooms.js");
-const updateRooms = require("./Admin/updateRooms.js");
-const bookingRouter = require("./Users/booking.js");
-const addRooms = require("./Admin/addRooms.js");
->>>>>>> 37329f292fc201c8ef86d8be5546e7e08c7270e9
 
-const cors = require("cors");
+const cors = require("cors")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 app.use('/' , usersignup);
 app.use('/api' , usersignin);
 app.use('/admin' , addRooms)
 app.use('/admin' , adminauth)
 app.use('/admin' , deleteRooms)             
 app.use('/admin' , updateRooms)
-app.use('/admin' , viewBookings)
+app.use('/admin' , adminViewBookings)
 app.use('/HotelApi' , allHotelData)
 app.use('/booking' , bookingRouter)
-=======
-app.use("/", usersignup);
-app.use("/api", usersignin);
-app.use("/admin", addRooms);
-app.use("/admin", adminauth);
-app.use("/admin", deleteRooms);
-app.use("/admin", updateRooms);
-app.use("/HotelApi", allHotelData);
-app.use("/booking", bookingRouter);
->>>>>>> 37329f292fc201c8ef86d8be5546e7e08c7270e9
+app.use("/admin" , adminCancelBooking)
 
-app.listen(3000, function () {
-  //("Everything is working fine");
-});
+app.listen(3000 , function(){
+    console.log("Everything is working fine");
+    
+})
