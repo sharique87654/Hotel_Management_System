161678c66d16620c444b665317352366e64b6f76
Sadique Ali <sadiqueali7488@gmail.com>
2025-11-08 15:21:24 +0530

    resolved index issue

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const usersignup = require("./Auth/routes/signup.js");
-const usersignin = require("./Auth/routes/signin.js");
-const adminauth = require("./Admin/auth.js");
-const allHotelData = require("./Admin/allHotelData.js");
-const deleteRooms = require("./Admin/deleteRooms.js");
-const updateRooms = require("./Admin/updateRooms.js");
-const bookingRouter = require("./Users/booking.js");
-const addRooms = require("./Admin/addRooms.js");
+const express = require('express');
+const usersignup = require("./Auth/routes/signup.js")
+const usersignin = require("./Auth/routes/signin.js")
+const adminauth = require("./Admin/auth.js")
+const allHotelData = require("./Admin/allHotelData.js")
+const deleteRooms = require("./Admin/deleteRooms.js")
+const updateRooms = require("./Admin/updateRooms.js")
+const bookingRouter = require("./Users/booking.js")
+const addRooms = require("./Admin/addRooms.js")  
+const viewBookings = require("./Admin/viewBooking.js");
 
-const cors = require("cors");
+
+const cors = require("cors")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", usersignup);
-app.use("/api", usersignin);
-app.use("/admin", addRooms);
-app.use("/admin", adminauth);
-app.use("/admin", deleteRooms);
-app.use("/admin", updateRooms);
-app.use("/HotelApi", allHotelData);
-app.use("/booking", bookingRouter);
+app.use('/' , usersignup);
+app.use('/api' , usersignin);
+app.use('/admin' , addRooms)
+app.use('/admin' , adminauth)
+app.use('/admin' , deleteRooms)             
+app.use('/admin' , updateRooms)
+app.use('/admin' , viewBookings)
+app.use('/HotelApi' , allHotelData)
+app.use('/booking' , bookingRouter)
 
-app.listen(3000, function () {
-  //("Everything is working fine");
-});
+
+app.listen(3000 , function(){
+    console.log("Everything is working fine");
+})
