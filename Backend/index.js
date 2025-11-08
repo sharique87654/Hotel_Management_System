48966787342341d679b5495d5b2e6391a93dc8b2
Sadique Ali <sadiqueali7488@gmail.com>
2025-11-08 12:31:01 +0530

    Resolved conflict

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const usersignup = require("./Auth/routes/signup.js")
-const usersignin = require("./Auth/routes/signin.js")
-const adminauth = require("./Admin/auth.js")
-const allHotelData = require("./Admin/allHotelData.js")
-const deleteRooms = require("./Admin/deleteRooms.js")
-const updateRooms = require("./Admin/updateRooms.js")
-const bookingRouter = require("./Users/booking.js")
-const addRooms = require("./Admin/addRooms.js")  
-const viewBookings = require("./Admin/viewBooking.js");
-
-=======
 const express = require("express");
 const usersignup = require("./Auth/routes/signup.js");
 const usersignin = require("./Auth/routes/signin.js");
@@ -20,7 +7,6 @@ const deleteRooms = require("./Admin/deleteRooms.js");
 const updateRooms = require("./Admin/updateRooms.js");
 const bookingRouter = require("./Users/booking.js");
 const addRooms = require("./Admin/addRooms.js");
->>>>>>> 37329f292fc201c8ef86d8be5546e7e08c7270e9
 
 const cors = require("cors");
 const app = express();
@@ -28,17 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-app.use('/' , usersignup);
-app.use('/api' , usersignin);
-app.use('/admin' , addRooms)
-app.use('/admin' , adminauth)
-app.use('/admin' , deleteRooms)             
-app.use('/admin' , updateRooms)
-app.use('/admin' , viewBookings)
-app.use('/HotelApi' , allHotelData)
-app.use('/booking' , bookingRouter)
-=======
 app.use("/", usersignup);
 app.use("/api", usersignin);
 app.use("/admin", addRooms);
@@ -47,7 +22,6 @@ app.use("/admin", deleteRooms);
 app.use("/admin", updateRooms);
 app.use("/HotelApi", allHotelData);
 app.use("/booking", bookingRouter);
->>>>>>> 37329f292fc201c8ef86d8be5546e7e08c7270e9
 
 app.listen(3000, function () {
   //("Everything is working fine");
